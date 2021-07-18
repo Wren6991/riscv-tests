d4a31956fa85ffbc39037f11b9b6ca550e6f2072
Luke Wren <wren6991@gmail.com>
2021-07-18 14:01:10 +0100

    Squash of hazard3 changes:
    Add hazard3 simulation target scripts
    
    Add 0xc0-byte vector dummy to start of test images for consistency with other hazard3 tests
    
    Hack timer IRQ test to run in hazard3 testbench (timer addresses, timeout length); reduce download test size to reduce test runtime.
    
    Add X bit to misa
    
    Add A bit to misa for Hazard3
    
    Fix reset vector for hazard3
    
    Add U bit to misa value
    
    Fix PrivRw test failing on systems with PMP due to trap changing privilege
    
    Remove PMP hack from PrivRw test. Next commit will add a better solution.
    
    Fix GdbTest.disable_pmp failing on systems which support NAPOT but not TOR regions

---
+++ debug/targets/luke/hazard3.py
@@ -0,0 +1,24 @@
+import targets
+import testlib
+
+# Based on spike32.py
+
+class hazard3_hart(targets.Hart):
+    xlen = 32
+    ram = 0x00000000
+    ram_size = 128 * 1024 # amount present on iCEBreaker -- can be more on CXXRTL sim.
+    bad_address = 0xf00ba700
+    instruction_hardware_breakpoint_count = 0
+    reset_vectors = [0x40]
+    link_script_path = "hazard3.lds"
+    misa = 0x40901105 # 32IMACX + U (X because of nonstandard debug CSRs)
+
+
+class hazard3(targets.Target):
+    harts = [hazard3_hart()]
+    openocd_config_path = "hazard3-remote-bitbang.cfg"
+    timeout_sec = 300
+    support_memory_sampling = False # Needs SBA
+
+    def create(self):
+        return testlib.GenericSimCmd(sim_cmd=self.sim_cmd)
