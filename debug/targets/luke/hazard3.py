91329aa8c23fe1317b035c2dd30681f29db3ef23
Luke Wren <wren6991@gmail.com>
2021-12-04 11:16:48 +0000

    Add X bit to misa

--- debug/targets/luke/hazard3.py
+++ debug/targets/luke/hazard3.py
@@ -11,7 +11,7 @@ class hazard3_hart(targets.Hart):
     instruction_hardware_breakpoint_count = 0
     reset_vectors = [0xc0]
     link_script_path = "hazard3.lds"
-    misa = 0x40001104 # 32IMC
+    misa = 0x40801104 # 32IMCX (X because of nonstandard debug CSRs)
 
 
 class hazard3(targets.Target):
