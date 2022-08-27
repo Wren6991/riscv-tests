38ddd8687433cf64108d895b57f431c4ebdd4014
Luke Wren <wren6991@gmail.com>
2022-08-27 17:04:22 +0100

    Enable hardware instruction breakpoints (oops forgot to commit this)

--- debug/targets/luke/hazard3.py
+++ debug/targets/luke/hazard3.py
@@ -8,7 +8,7 @@ class hazard3_hart(targets.Hart):
     ram = 0x00000000
     ram_size = 128 * 1024 # amount present on iCEBreaker -- can be more on CXXRTL sim.
     bad_address = 0xf00ba700
-    instruction_hardware_breakpoint_count = 0
+    instruction_hardware_breakpoint_count = 4
     reset_vectors = [0x40]
     link_script_path = "hazard3.lds"
     misa = 0x40901105 # 32IMACX + U (X because of nonstandard debug CSRs)
