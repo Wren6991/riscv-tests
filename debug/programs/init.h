4e48c599df6fddbd5fe09145f05e0b8b2ed99119
Luke Wren <wren6991@gmail.com>
2025-10-21 23:11:54 +0100

    Update memory map for new tb memory layout

--- debug/programs/init.h
+++ debug/programs/init.h
@@ -5,8 +5,8 @@
 // #define MTIME           (*(volatile long long *)(CLINT + 0xbff8))
 // #define MTIMECMP        ((volatile long long *)(CLINT + 0x4000))
 // different hardcoded values for hazard3 tb
-#define MTIME           (*(volatile long long *)(0x80000100))
-#define MTIMECMP        ((volatile long long *)(0x80000108))
+#define MTIME           (*(volatile long long *)(0xc0000100))
+#define MTIMECMP        ((volatile long long *)(0xc0000108))
 #endif
 
 typedef void* (*trap_handler_t)(unsigned hartid, unsigned mcause, void *mepc,
--- debug/targets/luke/hazard3.py
+++ debug/targets/luke/hazard3.py
@@ -5,7 +5,7 @@ import testlib
 
 class hazard3_hart(targets.Hart):
     xlen = 32
-    ram = 0x00000000
+    ram = 0x80000000
     ram_size = 128 * 1024 # amount present on iCEBreaker -- can be more on CXXRTL sim.
     bad_address = 0xf00ba700
     instruction_hardware_breakpoint_count = 4
