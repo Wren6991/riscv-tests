cbd94d17b5e39eb154fba43191e028f1b6da33ab
Luke Wren <wren6991@gmail.com>
2025-10-28 18:10:12 +0000

    Update hardcoded CLINT timer address in RepeatReadTest

--- debug/gdbserver.py
+++ debug/gdbserver.py
@@ -998,7 +998,7 @@ class RepeatReadTest(DebugTest):
     def test(self):
         self.gdb.b("main:start")
         self.gdb.c()
-        mtime_addr = 0x80000100 # changed hardcoded value for hazard3
+        mtime_addr = 0xc0000100 # changed hardcoded value for hazard3
         count = 1024
         output = self.gdb.command(
             f"monitor riscv repeat_read {count} 0x{mtime_addr:x} 4")
